acfd92e3585ca47c6f08cdf0069fde25887f2f41
sunny prajapati <sunnyprajapati9761@gmail.com>
2024-12-26 18:18:09 +0530

    added css

--- src/components/HangmanPage.jsx
+++ src/components/HangmanPage.jsx
@@ -6,14 +6,27 @@ import { selectAllState } from '../Redux/HangmanSlice'
 function HangmanPage() {
     const allState=useSelector(selectAllState)
   return (
-    <div>
-    <h1>Hint:-{allState.randomCategory}</h1>
-    <h1>Guess Word:{allState.randomHintWord}</h1>
-    <h1>Maximum Attempt: {allState.maxAttepts}</h1>
-    <h1>Score : {allState.score}</h1>
-      <MatchCharVeiw/>
+   
+    <div className="flex flex-col items-center bg-gray-50 p-6 rounded-lg shadow-md max-w-xl mx-auto">
+      <h1 className="text-xl font-semibold text-gray-800 mb-2">
+        Hint: <span className="text-blue-600">{allState.randomCategory}</span>
+      </h1>
+      <h1 className="text-xl font-semibold text-gray-800 mb-2">
+        Guess Word:{" "}
+        <span className="text-green-600">{allState.randomHintWord}</span>
+      </h1>
+      <h1 className="text-lg text-gray-700 mb-2">
+        Maximum Attempts:{" "}
+        <span className="text-red-600">{allState.maxAttepts}</span>
+      </h1>
+      <h1 className="text-lg text-gray-700 mb-4">
+        Score: <span className="text-yellow-500">{allState.score}</span>
+      </h1>
+      <div className="w-full">
+        <MatchCharVeiw />
+      </div>
     </div>
-  )
+  );
 }
 
 export default HangmanPage
